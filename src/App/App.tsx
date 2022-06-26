f33f304dbbc831972ea5f32eb274088b7b33854f
jose <vazquezjluis@yahoo.com>
2022-06-26 02:21:45 -0300

    add navbar, leftmenu, body

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,25 +1,9 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Leftmenu } from "../components/Leftmenu";
-import { Navbar } from "../components/Navbar";
-import { ApplicationState, Dispatch } from "../store";
+import { ContainerApp } from "../components/ContainerApp";
 
 export const App: React.FC = () => {
-  const { lista_usuarios } = useSelector((state: ApplicationState) => ({
-    lista_usuarios: state.usuarios,
-  }));
-  const { usuarios } = useDispatch<Dispatch>();
-  useEffect(() => {
-    usuarios.fetch();
-  }, []);
-
   return (
     <div>
-      <Navbar />
-      <Leftmenu />
-      email: {lista_usuarios.email}
-      <br></br>
-      clave: {lista_usuarios.clave}
+      <ContainerApp />
     </div>
   );
 };
--- src/components/Leftmenu/Leftmenu.tsx
+++ src/components/Leftmenu/Leftmenu.tsx
@@ -1,68 +1,70 @@
-import {
-  Collapse,
-  List,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  ListSubheader,
-} from "@mui/material";
-import {
-  Send,
-  Drafts,
-  ExpandLess,
-  ExpandMore,
-  StarBorder,
-  Inbox,
-} from "@mui/icons-material";
-import { useState } from "react";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import ListSubheader from "@mui/material/ListSubheader";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import PeopleIcon from "@mui/icons-material/People";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import LayersIcon from "@mui/icons-material/Layers";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import { Divider, List } from "@mui/material";
 
-export const Leftmenu = () => {
-  const [open, setOpen] = useState(true);
-
-  const handleClick = () => {
-    setOpen(!open);
-  };
-
+export const Leftmenu = ({ open, toggleDrawer }) => {
   return (
-    <List
-      sx={{ width: "100%", maxWidth: 360 }}
-      component="nav"
-      aria-labelledby="nested-list-subheader"
-      subheader={
-        <ListSubheader component="div" id="nested-list-subheader">
-          Nested List Items
-        </ListSubheader>
-      }
-    >
+    <List component="nav">
       <ListItemButton>
         <ListItemIcon>
-          <Send />
+          <DashboardIcon />
         </ListItemIcon>
-        <ListItemText primary="Sent mail" />
+        <ListItemText primary="Dashboard" />
       </ListItemButton>
       <ListItemButton>
         <ListItemIcon>
-          <Drafts />
+          <ShoppingCartIcon />
         </ListItemIcon>
-        <ListItemText primary="Drafts" />
+        <ListItemText primary="Orders" />
       </ListItemButton>
-      <ListItemButton onClick={handleClick}>
+      <ListItemButton>
         <ListItemIcon>
-          <Inbox />
+          <PeopleIcon />
         </ListItemIcon>
-        <ListItemText primary="Inbox" />
-        {open ? <ExpandLess /> : <ExpandMore />}
+        <ListItemText primary="Customers" />
       </ListItemButton>
-      <Collapse in={open} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding>
-          <ListItemButton sx={{ pl: 4 }}>
-            <ListItemIcon>
-              <StarBorder />
-            </ListItemIcon>
-            <ListItemText primary="Starred" />
-          </ListItemButton>
-        </List>
-      </Collapse>
+      <ListItemButton>
+        <ListItemIcon>
+          <BarChartIcon />
+        </ListItemIcon>
+        <ListItemText primary="Reports" />
+      </ListItemButton>
+      <ListItemButton>
+        <ListItemIcon>
+          <LayersIcon />
+        </ListItemIcon>
+        <ListItemText primary="Integrations" />
+      </ListItemButton>
+      <Divider sx={{ my: 1 }} />
+      <ListSubheader component="div" inset>
+        Saved reports
+      </ListSubheader>
+      <ListItemButton>
+        <ListItemIcon>
+          <AssignmentIcon />
+        </ListItemIcon>
+        <ListItemText primary="Current month" />
+      </ListItemButton>
+      <ListItemButton>
+        <ListItemIcon>
+          <AssignmentIcon />
+        </ListItemIcon>
+        <ListItemText primary="Last quarter" />
+      </ListItemButton>
+      <ListItemButton>
+        <ListItemIcon>
+          <AssignmentIcon />
+        </ListItemIcon>
+        <ListItemText primary="Year-end sale" />
+      </ListItemButton>
     </List>
   );
 };
