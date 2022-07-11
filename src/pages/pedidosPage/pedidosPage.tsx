f9ae1571febb1be085aae4efb4639ffeee326c95
jose <vazquezjluis@yahoo.com>
2022-07-11 09:16:21 -0300

    pedidos

--- src/pages/pedidosPage/pedidosPage.tsx
+++ src/pages/pedidosPage/pedidosPage.tsx
@@ -1,5 +1,9 @@
-import React from "react";
+import React from 'react'
 
-export const pedidosPage = () => {
-  return <>test</>;
-};
+const PedidosPage = () => {
+  return (
+    <div>PedidosPage</div>
+  )
+}
+
+export default PedidosPage
