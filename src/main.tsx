21615bbd37eedb0ca1da917ffe458de3e78fcdc5
jose luis vazquez <vazquezjoseluis0508@gmail.com>
2023-02-28 23:34:15 -0300

    hash route

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { darkTheme } from './styling/themeConfig'
 import {
   QueryClientProvider
 } from '@tanstack/react-query'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import App from './App'
 import queryClient from './queryClient'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
@@ -20,10 +20,10 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 
     <ThemeProvider theme={darkTheme}>
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
+        <HashRouter>
           <App />
           <ReactQueryDevtools  initialIsOpen={false} />
-        </BrowserRouter>
+        </HashRouter>
       </QueryClientProvider>
     </ThemeProvider>
 )
