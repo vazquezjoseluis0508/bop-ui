037fc683b908c69929b4f1850f77fed7aa125a5c
jose luis vazquez <vazquezjoseluis0508@gmail.com>
2023-05-27 10:48:30 -0300

    modificacin de certificados

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3001,
+    https: {
+      key: fs.readFileSync('./key.pem'),
+      cert: fs.readFileSync('./cert.pem'),
+    },
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
